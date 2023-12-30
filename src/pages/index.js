1ff9b932f6046874045b09e48997d3764694e00c
katawut <katawutu.io@gmail.com>
2023-12-30 14:14:05 +0700

    update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ export default function IndexPage() {
   return(
     <>
       <h1>New Born demo</h1>
+      <h2 style={{color: 'pink'}}>Hello Proud! This is our business website</h2>
       <p>When firstclass digital services meet your business</p>
     </>
   );
